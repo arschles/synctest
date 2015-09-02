246de0eda71bf403b4c7c9ba0627fbc912a56396
Aaron Schlesinger <arschles@gmail.com>
2015-09-02 12:31:24 -0700

    using simple slice of channels to close. also improving tests

--- locker.go
+++ locker.go
@@ -5,55 +5,60 @@ import "sync"
 // Locker is an implementation of sync.Locker that notifies callers when
 // locks and unlocks happen. otherwise, it behaves identically as a sync.Mutex
 type NotifyingLocker struct {
-	cond   *sync.Cond
-	locked bool
-	lck    *sync.Mutex
+	unlockChans     []chan struct{}
+	unlockChansLock *sync.Mutex
+	lockChans       []chan struct{}
+	lockChansLock   *sync.Mutex
+	lck             *sync.Mutex
 }
 
 func NewNotifyingLocker() *NotifyingLocker {
-	return &NotifyingLocker{cond: sync.NewCond(&sync.Mutex{}), locked: false, lck: &sync.Mutex{}}
+	return &NotifyingLocker{
+		unlockChans:     nil,
+		unlockChansLock: &sync.Mutex{},
+		lockChans:       nil,
+		lockChansLock:   &sync.Mutex{},
+		lck:             &sync.Mutex{},
+	}
 }
 
 // NotifyLock returns a channel that will close when n is locked. The channel
 // never sends and will be closed immediately if n is already locked
 func (n *NotifyingLocker) NotifyLock() <-chan struct{} {
+	n.lockChansLock.Lock()
+	defer n.lockChansLock.Unlock()
 	ch := make(chan struct{})
-	go func() {
-		defer close(ch)
-		n.cond.L.Lock()
-		for !n.locked {
-			n.cond.Wait()
-		}
-	}()
+	n.lockChans = append(n.lockChans, ch)
 	return ch
 }
 
 // NotifyUnlock returns a channel that will close when n is unlocked. The channel
 // never sends and will be closed immediately if n is already unlocked.
 func (n *NotifyingLocker) NotifyUnlock() <-chan struct{} {
+	n.unlockChansLock.Lock()
+	defer n.unlockChansLock.Unlock()
 	ch := make(chan struct{})
-	go func() {
-		defer close(ch)
-		n.cond.L.Lock()
-		for n.locked {
-			n.cond.Wait()
-		}
-	}()
+	n.unlockChans = append(n.unlockChans, ch)
 	return ch
 }
 
+// Lock locks n and closes all unclosed channels returned previously by NotifyLock
 func (n *NotifyingLocker) Lock() {
 	n.lck.Lock()
-	n.cond.L.Lock()
-	defer n.cond.L.Unlock()
-	n.locked = true
-	n.cond.Broadcast()
+	n.lockChansLock.Lock()
+	defer n.lockChansLock.Unlock()
+	for _, lck := range n.lockChans {
+		close(lck)
+	}
+	n.lockChans = nil
 }
 
 func (n *NotifyingLocker) Unlock() {
 	n.lck.Unlock()
-	n.cond.L.Lock()
-	defer n.cond.L.Unlock()
-	n.locked = false
-	n.cond.Broadcast()
+	n.unlockChansLock.Lock()
+	defer n.unlockChansLock.Unlock()
+	for _, lck := range n.unlockChans {
+		close(lck)
+	}
+	n.unlockChans = nil
 }
--- locker_test.go
+++ locker_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	timeout = 1 * time.Second
+	timeout = 100 * time.Millisecond
 )
 
 func TestNotifyLock(t *testing.T) {
@@ -26,7 +26,54 @@ func TestNotifyLockAlreadyLocked(t *testing.T) {
 	ch := nl.NotifyLock()
 	select {
 	case <-ch:
+		t.Fatalf("received on channel")
 	case <-time.After(timeout):
-		t.Fatalf("didn't receive on channel after %s", timeout)
+	}
+}
+
+func TestNotifyLockOnUnlock(t *testing.T) {
+	nl := NewNotifyingLocker()
+	nl.Lock()
+	ch := nl.NotifyLock()
+	nl.Unlock()
+	select {
+	case <-ch:
+		t.Fatalf("got a lock notification on unlock")
+	case <-time.After(timeout):
+	}
+}
+
+func TestNotifyUnlock(t *testing.T) {
+	nl := NewNotifyingLocker()
+	ch := nl.NotifyUnlock()
+	nl.Lock()
+	nl.Unlock()
+	select {
+	case <-ch:
+	case <-time.After(timeout):
+		t.Fatalf("didn't get unlock notification after %s", timeout)
+	}
+}
+
+func TestNotifyUnlockAlreadyUnlocked(t *testing.T) {
+	nl := NewNotifyingLocker()
+	nl.Lock()
+	nl.Unlock()
+	ch := nl.NotifyUnlock()
+	select {
+	case <-ch:
+		t.Fatalf("got unlock notification")
+	case <-time.After(timeout):
+	}
+}
+
+func TestNofifyUnlockOnLock(t *testing.T) {
+	nl := NewNotifyingLocker()
+	ch := nl.NotifyUnlock()
+	nl.Lock()
+	select {
+	case <-ch:
+		t.Fatalf("got unlock notification on lock")
+	case <-time.After(timeout):
 	}
 }
